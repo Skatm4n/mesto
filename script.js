25276b5fd860d466d5f8e64ff5f63c9312ec06ab
Iskander Mukhametgaleev <iskander150801@gmail.com>
2021-12-10 18:02:39 +0500

    fix: first review

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-//Открытие и закрытие окна изменения
 const editButton = document.querySelector('.avatar__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-btn');
+const saveButton = document.querySelector('.popup__form-btn');
+const profileName = document.querySelector('.avatar__name');
+const profileDescription = document.querySelector('.avatar__description');
+const nameValue = document.querySelector('.popup__form-item-name');
+const descriptionValue = document.querySelector('.popup__form-item-description');
 
-function popupVisible(){
+function openPopup(){
   popup.classList.add('popup_visible');
 }
 
@@ -11,28 +15,17 @@ function closePopup (){
   popup.classList.remove('popup_visible');
 }
 
-editButton.addEventListener('click', popupVisible);
-closeButton.addEventListener('click', closePopup);
-
-
-//Изменение имени и описания профиля
-const saveButton = document.querySelector('.popup__form-btn');
-const profileName = document.querySelector('.avatar__name');
-const profileDescription = document.querySelector('.avatar__description');
-
 function changeData (def){
   def.preventDefault();
-  let nameValue = document.querySelector('.popup__form-item-name').value;
-  console.log(nameValue);
-  let descriptionValue = document.querySelector('.popup__form-item-description').value;
-  console.log(descriptionValue);
 
-  profileName.textContent = nameValue;
-  profileDescription.textContent = descriptionValue;
+  profileName.textContent = nameValue.value;
+  profileDescription.textContent = descriptionValue.value;
 
   closePopup();
 }
 
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 saveButton.addEventListener('click', changeData);
 
 
